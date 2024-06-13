8f28ec429027f76b906940f1b4d16d0ccdb37648
VinniVieira <viniciusmcvieira@gmail.com>
2024-06-13 20:54:45 -0300

    formt

---
+++ Aula de QA/aula1.py
@@ -0,0 +1,11 @@
+nome = "Paula Matins"
+mes = "Janeiro"
+Valor = 500,00
+Desconto = 10
+cupom = "PAULAÉ10"
+
+mensagem1 = f"O nome da pessoa é {nome}"
+mensagem2 = "O nome da pessoa é {}".format (nome)
+mensagem3 = "O nome da pessoa é {a}".format (a=nome, b=mes)
+
+print = ("mensagem1")
