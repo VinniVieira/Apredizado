482575dece43578fac7362f536f9fba881334f86
VinniVieira <viniciusmcvieira@gmail.com>
2024-06-13 19:15:59 -0300

    ATT

--- Aula de QA/novoarqui.py
+++ Aula de QA/novoarqui.py
@@ -1,1 +1,10 @@
-print("Helo Word")
+# var no py não precisa chamar "var" é colocar direto, por exemplo nome --> nome = "vinicius"
+
+
+nome = "vinicius"
+idade = 20
+saldo = -100,00
+
+print ("Olá " + nome + "sua idade é" + idade + "seu saldo bancario é:" + saldo)
+
+#tipos de dados: inteiro(Numeros), boleano (Numeros fracionarios), string(Texto""") e flowt ()
