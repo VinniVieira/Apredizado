d24dc13c736c942ce8846a26eebf1e865f0ff0c9
VinniVieira <viniciusmcvieira@gmail.com>
2024-06-13 21:24:47 -0300

    inputs

---
+++ Aula de QA/input.py
@@ -0,0 +1,7 @@
+nome = input ("digit seu nome: ")
+idade = int(input ("digit sua idade: "))
+
+# é possivel alterar o tipo de dado de uma var -> idade_formatada = str (idade) -> tranforma a vaar de idade em uma string
+
+
+print(f"o nome da pessoa é {nome}, e a idade é: {idade}") 
